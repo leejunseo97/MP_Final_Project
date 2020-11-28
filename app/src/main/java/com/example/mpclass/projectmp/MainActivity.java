c038c7e776bc9c601ddada77ed4ad4175f95a276
leejunseo97 <junseolee97@gmail.com>
2020-11-28 16:32:26 +0900

    AndroidManifest.xml 카메라 관련 옵션 설정해줌

--- app/src/main/java/com/example/mpclass/projectmp/MainActivity.java
+++ app/src/main/java/com/example/mpclass/projectmp/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
-    //test git
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
